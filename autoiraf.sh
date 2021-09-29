8eae52751624ca99a9384a06a6aefca46a497f1e
bmccole <bambimccole@gmail.com>
2021-09-29 08:24:12 -0500

    Update autoiraf.sh

--- autoiraf.sh
+++ autoiraf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#Copyright (C) 2021  Bambi A. McCole
-#email msbam@msbam.space
+# Copyright (C) 2021  Bambi A. McCole
+# email: msbam@msbam.space
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
